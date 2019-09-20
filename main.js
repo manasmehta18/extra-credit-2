81da64a509b70f0e1a54676b9f80546f207dd3f8
manasmehta18 <manasmehta.18@gmail.com>
2019-09-20 02:29:33 -0400

    added acdc

--- main.js
+++ main.js
@@ -55,6 +55,63 @@ const startMello = function() {
     draw()
 }
 
+const startAC = function() {
+    document.getElementById('canvas').innerHTML = ''
+    const canvas = document.createElement( 'canvas' )
+    document.getElementById('canvas').appendChild( canvas )
+    canvas.width = 1500;
+    canvas.height = 800;
+    const ctx = canvas.getContext( '2d' )
+
+    // audio init
+    const audioCtx = new AudioContext()
+    const audioElement = document.createElement( 'audio' )
+    document.getElementById('canvas').appendChild( audioElement )
+
+    // audio graph setup
+    const analyser = audioCtx.createAnalyser()
+    analyser.fftSize = 2048;
+    const player = audioCtx.createMediaElementSource( audioElement )
+    player.connect( audioCtx.destination )
+    player.connect( analyser )
+
+    audioElement.src = 'media/acdc.mp3';
+    audioElement.play()
+
+    const results = new Uint8Array( analyser.frequencyBinCount )
+
+    draw = function() {
+        // temporal recursion, call tthe function in the future
+        window.requestAnimationFrame( draw )
+
+        // ctx.fillStyle = 'black'
+        // ctx.fillRect( 0,0,canvas.width,canvas.height )
+        // ctx.fillStyle = 'white'
+
+        analyser.getByteFrequencyData( results )
+
+        let x = 0;
+
+        ctx.fillStyle = "black";
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+        for( let i = 0; i < analyser.frequencyBinCount; i++ ) {
+            //ctx.fillRect( i, 0, 1, results[i] ) // upside down
+            let barHeight = results[i] * 2 + 60;
+
+            var r = barHeight + (25 * (i/analyser.frequencyBinCount));
+            var g = 250 * (i/analyser.frequencyBinCount);
+            var b = 50;
+
+            ctx.fillStyle = "rgb(" + g+ "," + r + "," + b + ")";
+            ctx.fillRect(x, canvas.height - barHeight, (canvas.width/analyser.frequencyBinCount) * 2.5, barHeight);
+
+            x += (canvas.width/analyser.frequencyBinCount) * 2.5 + 1;
+        }
+    }
+    draw()
+}
+
 const startElectro = function() {
     document.getElementById('canvas').innerHTML = ''
     const canvas = document.createElement( 'canvas' )
@@ -103,7 +160,7 @@ const startElectro = function() {
             var g = 250 * (i/analyser.frequencyBinCount);
             var b = 50;
 
-            ctx.fillStyle = "rgb(" + g + "," + r + "," + b + ")";
+            ctx.fillStyle = "rgb(" + r + "," + g + "," + r + ")";
             ctx.fillRect(x, canvas.height - barHeight, (canvas.width/analyser.frequencyBinCount) * 2.5, barHeight);
 
             x += (canvas.width/analyser.frequencyBinCount) * 2.5 + 1;
@@ -347,4 +404,5 @@ window.onload = function() {
     document.getElementById('instru').onclick = startInst;
     document.getElementById('beats').onclick = startBeat;
     document.getElementById('dubstep').onclick = startDub;
+    document.getElementById('acdc').onclick = startAC;
 }
